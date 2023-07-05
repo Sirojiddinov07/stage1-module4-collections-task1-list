25966458cf5e249a9f108b220701e23654dd5c06
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-07-05 13:26:41 +0500

    first commit

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> rest = new ArrayList<>();
+        int i = 1;
+        for (String str : sourceList) {
+            if (i++ % 3 == 0) {
+                rest.add(str);
+                rest.add(str);
+            }
+        }
+        return rest;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aInt = Integer.parseInt(a);
+        int bInt = Integer.parseInt(b);
+        if (aInt * aInt > bInt * bInt)
+            return 1;
+        else if (aInt * aInt < bInt * bInt)
+            return -1;
+        else
+            return Integer.compare(aInt, bInt);
     }
 }
